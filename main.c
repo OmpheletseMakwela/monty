8402caa2d8ab6ad8a3de13f0c75a7597fd7fd793
Ompheletse Makwela <omphentse@gmail.com>
2023-12-14 09:39:47 +0200

    update

--- main.c
+++ main.c
@@ -4,55 +4,57 @@
 #include <string.h>
 #include "monty.h"
 
-/**
- * exec_opcodes - A function that executes opcodes
- * @file: Pointer to a file
- * @stack: Double pointer to a stack_t structure
- * Return- nothing
- */
-void exec_opcodes(FILE *file, stack_t **stack) {
+int main(int argc, char *argv[]) {
+    FILE *file;
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
     unsigned int line_number = 1;
+    stack_t *stack = NULL;
+
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
 
     while ((read = getline(&line, &len, file)) != -1) {
         char *token = strtok(line, " \n");
-
-        while (token != NULL) {
-            opcode_process(stack, token, line_number);
-            token = strtok(NULL, " \n");
+        if (token != NULL) {
+            if (strcmp(token, "push") == 0) {
+                char *argument = strtok(NULL, " \n");
+                 int value = atoi(argument);
+                if (argument == NULL) {
+                    fprintf(stderr, "L%u: usage: push integer\n", line_number);
+                    fclose(file);
+                    free(line);
+                    return EXIT_FAILURE;
+               }
+                push_node(&stack, value);
+            } else if (strcmp(token, "pall") == 0) {
+                pall(&stack, line_number);
+            }else if (strcmp(token, "pint") == 0) {
+    pint(&stack, line_number);
+            }else if (strcmp(token, "pop") == 0){
+                    pop(&stack, line_number);
+            }
+            else {
+                fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
+                fclose(file);
+                free(line);
+                return EXIT_FAILURE;
+            }
         }
         line_number++;
     }
+
     free(line);
     fclose(file);
+
+    return EXIT_SUCCESS;
 }
-
-
-/**
- * main - main function
- * @argc: first argument of a function
- * @argv: Pointer to character
- * Return- EXIT SUCCESS
- */
-int main(int argc, char *argv[])
-{
-	stack_t *stack = NULL;
-	FILE *file = fopen(argv[1], "r");
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-	}
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
-	}
-	exec_opcodes(file, &stack);
-	fclose(file);
-	return (EXIT_SUCCESS);
-}
-
